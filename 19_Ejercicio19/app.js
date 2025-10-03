fd128eb95921311136fe836a9343a4ff89938b7c
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-10-03 14:05:56 -0500

    mini rpg automatizado

--- 19_Ejercicio19/app.js
+++ 19_Ejercicio19/app.js
@@ -1,4 +1,4 @@
-class entithy {
+class Entithy {
     constructor(nombre, vida, vidaMax, ataque, denfesa){
         this.nombre = nombre;
         this.vida = vida;
@@ -7,10 +7,23 @@ class entithy {
         this.defensa = denfesa;
     }
     atacar(objetivo) {
+        let chance = Math.random();
         let dano = this.ataque - objetivo.defensa;
-        dano < 0 ? dano = 0 : dano;
-        objetivo.recibirDano(dano);
-        return `${this.nombre} ataca a ${objetivo.nombre} y le hace ${dano} puntos de dano`;
+        let resultado = "";
+
+        if (chance < 0.15) {
+            dano = 0;
+            resultado = `${this.nombre} intenta atacar a ${objetivo.nombre} pero falla al atacar`;
+        } else if (chance > 0.85) {
+            dano = (dano < 0 ? 0 : dano) * 2;
+            objetivo.recibirDano(dano);
+            resultado = `${this.nombre} lanza un GOLPE CRITICO contra ${objetivo.nombre} haciendo ${dano} puntos de daño`;
+        } else {
+            dano = dano < 0 ? 0 : dano;
+            objetivo.recibirDano(dano);
+            resultado = `${this.nombre} ataca a ${objetivo.nombre} y le hace ${dano} puntos de dano`;
+        }
+        return resultado
     }
     recibirDano(cantidadDano){
         this.vida -= cantidadDano;
@@ -22,11 +35,23 @@ class entithy {
     }
 }
 
-const Jugador = new entithy("Jugador", 100, 100, 20, 10);
-const Enemigo = new entithy("Goblin", 80, 80, 15, 5);
+const Jugador = new Entithy('Jugador', 100, 100, 20, 10);
+const Enemigo = new Entithy('Goblin', 80, 80, 15, 5);
 
-console.log(Jugador.atacar(Enemigo));
-console.log(Enemigo.atacar(Jugador));
+const combatir = (ent1, ent2) => {
+    let turno = 1;
+    while (ent1.estaVivo() && ent2.estaVivo()) {
+        console.log(`\n--- Turno ${turno} ---`);
+        console.log(ent1.atacar(ent2));
+        if (!ent2.estaVivo()) break;
+        console.log(ent2.atacar(ent1));
+        turno += 1;
+    }
+    if (ent1.estaVivo()) {
+        console.log(`\n ${ent1.nombre} gana el combate!`)
+    } else {
+        console.log(`\n ${ent2.nombre} gana el combate!`)
+    }
+};
 
-console.log(Jugador);
-console.log(Enemigo);
+combatir(Jugador, Enemigo);
